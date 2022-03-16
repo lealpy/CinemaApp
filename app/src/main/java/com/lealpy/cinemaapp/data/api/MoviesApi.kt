d164901497d9316d0b08c854cba1fb7d8a6c2abd
lealpy <lealp@mail.ru>
2022-03-16 11:29:49 +0300

    added api logic

--- app/src/main/java/com/lealpy/cinemaapp/data/api/MoviesApi.kt
+++ app/src/main/java/com/lealpy/cinemaapp/data/api/MoviesApi.kt
@@ -1,4 +1,11 @@
 package com.lealpy.cinemaapp.data.api
 
+import com.lealpy.cinemaapp.data.models.ApiResponse
+import retrofit2.http.GET
+
 interface MoviesApi {
+
+    @GET("sequeniatesttask/films.json")
+    suspend fun getAllMovies(): ApiResponse
+
 }
